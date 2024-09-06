c0735a879aa856f52ff63a4185cea038ecd45ee9
sirvin01 <salimjr201@gmail.com>
2024-09-06 11:04:56 +0300

    V1 update

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,13 +1,33 @@
 from django.http import JsonResponse
+from django.forms import model_to_dict
 import json
+from products.models import Product
+
 
 def api_home(request,*args,**kwargs):
+     model_data = Product.objects.all().order_by("?").first()
+     data = {}
+     if model_data:
+          data = model_to_dict(model_data)# can add specific fields using fields= ["field1"]
+          """
+          the process below is complex .
+          model instance, turn it into python dict and return json to client
+          use the above instead
+          """
+        #   data['id'] = model_data.id
+        #   data['title'] = model_data.title
+        #   data['content'] = model_data.content
+        #   data['price'] = model_data.price
+     return JsonResponse(data)
 
-    body = request.body
-    data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data.keys)
-    return JsonResponse({"message":"Hello this is your Django API response"})
+
+# def api_home(request,*args,**kwargs):
+
+#     body = request.body
+#     data = {}
+#     try:
+#         data = json.loads(body)
+#     except:
+#         pass
+#     print(data.keys)
+#     return JsonResponse({"message":"Hello this is your Django API response"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint =  "http://127.0.0.1:8000/api"
 
-get_repsonse = requests.get(endpoint, params={"ABC":123}, json="Hello world")
+get_repsonse = requests.get(endpoint, json={"Product_id":124})
 
 print(get_repsonse.json())
 # print(get_repsonse.text)
