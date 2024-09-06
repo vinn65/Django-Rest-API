eb0851941b54a3408d5c8a233dc4c0ff0d83e94a
sirvin01 <salimjr201@gmail.com>
2024-09-06 13:17:59 +0300

    V2

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,24 +1,30 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from django.forms import model_to_dict
 import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
+@api_view(["GET"])
 def api_home(request,*args,**kwargs):
-     model_data = Product.objects.all().order_by("?").first()
+     instance = Product.objects.all().order_by("?").first()
      data = {}
-     if model_data:
-          data = model_to_dict(model_data)# can add specific fields using fields= ["field1"]
+     if instance:
+        #   data = model_to_dict(instance)# can add specific fields using fields= ["field1"]
+          data = ProductSerializer(instance).data
           """
           the process below is complex .
           model instance, turn it into python dict and return json to client
           use the above instead
           """
-        #   data['id'] = model_data.id
+        #   data['id'] = instance.id
         #   data['title'] = model_data.title
         #   data['content'] = model_data.content
         #   data['price'] = model_data.price
-     return JsonResponse(data)
+     return Response(data)
 
 
 # def api_home(request,*args,**kwargs):
